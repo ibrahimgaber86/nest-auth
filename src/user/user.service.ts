320abbc1b18bb2c8687ee36b285cb6d5bc4f7013
ibrahim gaber <ibrahimgaber190086@gamil.com>
2024-05-14 10:10:42 +0300

    create user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,46 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+import { genSalt, hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+
+    if (user) {
+      throw new ConflictException('user already exists');
+    }
+
+    const hashedPwd = await hash(createUserDto.password, await genSalt(10));
+    const { password, ...result } = await this.prisma.user.create({
+      data: { ...createUserDto, password: hashedPwd },
+    });
+
+    return result;
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: { ...updateUserDto },
+    });
+  }
+
+  remove(id: string) {
+    return this.prisma.user.delete({ where: { id } });
+  }
+}
